3754622c77ec0b3fe4870ebb45fd632bc6aaf06a
Artem Trubachev <trubachev.artem@gmail.com>
2018-07-22 16:37:12 +0200

    add responsive markup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
   startApp()
+
+  window.onresize = function(event) {
+    startApp()
+  }
 })
 
-
 function startApp() {
+  const clientWidth = document.documentElement.clientWidth
+  const segmentWidth = clientWidth / 16
+
+  const segmentsPositions = [
+    { x: "0px", y: "0px" }, // 0
+    { x: "0px", y: "0px" }, // 1
+    { x: `${segmentWidth}px`, y: "0px" }, // 2
+    { x: `${segmentWidth}px`, y: "0px" }, // 3
+    { x: "0px", y: `${segmentWidth}px` }, // 4
+    { x: "0px", y: `${segmentWidth}px` }, // 5
+    { x: `${segmentWidth}px`, y: `${segmentWidth}px` }, // 6
+    { x: `${segmentWidth}px`, y: `${segmentWidth}px` }, // 7
+    { x: "0px", y: `${segmentWidth * 2}px` }, // 8
+    { x: "0px", y: `${segmentWidth * 2}px` }, // 9
+    { x: `${segmentWidth}px`, y: `${segmentWidth * 2}px` }, // 10
+    { x: `${segmentWidth}px`, y: `${segmentWidth * 2}px` } // 11
+  ]
+
+  const createDigit = number => {
+    const digit = document.createElement("div")
+    digit.classList.add("digit")
+    digit.style.width = `${segmentWidth * 1.3}px`
+    digit.style.marginRight = `${segmentWidth * 0.7}px`
+
+    for (let i of Array(12).keys()) {
+      const segment = document.createElement("div")
+      segment.classList.add("segment")
+      segment.style.width = `${segmentWidth}px`
+      segment.style.height = `${segmentWidth * 0.3}px`
+      segment.style.left = segmentsPositions[i].x
+      segment.style.top = segmentsPositions[i].y
+      segment.style.transform = `rotate(${segmentAngles[number][i]}deg)`
+      segment.style.transformOrigin = `${segmentWidth * 0.15}px ${segmentWidth * 0.15}px`
+
+      const dot = document.createElement("div")
+      dot.classList.add("dot")
+      dot.style.width = `${segmentWidth * 0.3}px`
+      dot.style.height = `${segmentWidth * 0.3}px`
+
+      segment.appendChild(dot)
+
+      digit.appendChild(segment)
+    }
+    addCoversToDigit(digit, segmentWidth)
+
+    return digit
+  }
+
   const selectedTheme = findGetParameter("theme") || "white"
   const themes = ["white", "black", "orange", "mindal", "izettle", "pride"]
   const themesEl = document.getElementById("themes")
+  themesEl.innerHTML = ""
 
-  themes.forEach((theme) => {
+  themes.forEach(theme => {
     const themeEl = document.createElement("a")
     themeEl.href = `/?theme=${theme}`
     themeEl.innerHTML = theme
@@ -16,9 +68,9 @@ function startApp() {
     themesEl.appendChild(themeEl)
   })
 
-
   document.body.classList.add(selectedTheme)
   const clockDiv = document.getElementById("clock")
+  clockDiv.innerHTML = ""
 
   const now = new Date()
   const hours = now.getHours() % 12 || 12
@@ -27,10 +79,10 @@ function startApp() {
 
   const hours0 = createDigit(Math.floor(hours / 10))
   const hours1 = createDigit(hours % 10)
-  hours1.style.marginRight = "110px"
+  hours1.style.marginRight = `${segmentWidth * 1.1}px`
   const minutes0 = createDigit(Math.floor(minutes / 10))
   const minutes1 = createDigit(minutes % 10)
-  minutes1.style.marginRight = "110px"
+  minutes1.style.marginRight = `${segmentWidth * 1.1}px`
   const seconds0 = createDigit(Math.floor(seconds / 10))
   const seconds1 = createDigit(seconds % 10)
 
@@ -42,98 +94,74 @@ function startApp() {
   clockDiv.appendChild(seconds1)
 
   setInterval(() => {
-      const now = new Date()
-      const hours = now.getHours() % 12 || 12
-      const minutes = now.getMinutes()
-      const seconds = now.getSeconds()
+    const now = new Date()
+    const hours = now.getHours() % 12 || 12
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
 
-      setDigit(hours0, Math.floor(hours / 10))
-      setDigit(hours1, hours % 10)
-      setDigit(minutes0, Math.floor(minutes / 10))
-      setDigit(minutes1, minutes % 10)
-      setDigit(seconds0, Math.floor(seconds / 10))
-      setDigit(seconds1, seconds % 10)
-    },
-    1000
-  )
+    setDigit(hours0, Math.floor(hours / 10))
+    setDigit(hours1, hours % 10)
+    setDigit(minutes0, Math.floor(minutes / 10))
+    setDigit(minutes1, minutes % 10)
+    setDigit(seconds0, Math.floor(seconds / 10))
+    setDigit(seconds1, seconds % 10)
+  }, 1000)
 }
-
-const segmentsPositions = [
-  {x: "15px", y: "15px"},   // 0
-  {x: "15px", y: "15px"},   // 1
-  {x: "115px", y: "15px"},  // 2
-  {x: "115px", y: "15px"},  // 3
-  {x: "15px", y: "115px"},  // 4
-  {x: "15px", y: "115px"},  // 5
-  {x: "115px", y: "115px"}, // 6
-  {x: "115px", y: "115px"}, // 7
-  {x: "15px", y: "215px"},  // 8
-  {x: "15px", y: "215px"},  // 9
-  {x: "115px", y: "215px"}, // 10
-  {x: "115px", y: "215px"}, // 11
-]
 
 const segmentAngles = [
   // 0
-  [ 180, -90, 180, 90, -90, 180, 0, 90, -90, 0, 0, 90 ],
+  [180, -90, 180, 90, -90, 180, 0, 90, -90, 0, 0, 90],
   // 1
-  [ 180, -90, 135, 90, 180, -45, 0, 90, 180, 90, 0, 90 ],
+  [180, -90, 135, 90, 180, -45, 0, 90, 180, 90, 0, 90],
   // 2
-  [ 180, -90, 180, 90, 180, 180, 0, 135, -45, 0, 0, 90 ],
+  [180, -90, 180, 90, 180, 180, 0, 135, -45, 0, 0, 90],
   // 3
-  [ 180, 0, 180, 135, -45, 0, 135, 180, -45, 180, 0, 90 ],
+  [180, 0, 180, 135, -45, 0, 135, 180, -45, 180, 0, 90],
   // 4
-  [ 180, -90, 135, 90, -45, 0, 90, 0, 180, 90, 0, 90 ],
+  [180, -90, 135, 90, -45, 0, 90, 0, 180, 90, 0, 90],
   // 5
-  [ 180, -90, 180, 0, -90, 0, 180, 90, 180, 90, 180, 0 ],
+  [180, -90, 180, 0, -90, 0, 180, 90, 180, 90, 180, 0],
   // 6
-  [180, -90, 135, 135, -45, 0, 180, 90, -90, 0, 180, -90 ],
+  [180, -90, 135, 135, -45, 0, 180, 90, -90, 0, 180, -90],
   // 7
-  [ 180, 0, 180, 90, 180, 180, -90, 135, -45, 90, 0, 90 ],
+  [180, 0, 180, 90, 180, 180, -90, 135, -45, 90, 0, 90],
   // 8
-  [ 180, -90, 180, 90, -90, 0, 180, 90, -90, 0, 0, 90 ],
+  [180, -90, 180, 90, -90, 0, 180, 90, -90, 0, 0, 90],
   // 9
-  [ 0, 90, 180, 90, 180, 0, 180, 135, -45, -45, 0, 90]
+  [0, 90, 180, 90, 180, 0, 180, 135, -45, -45, 0, 90]
 ]
 
-function createDigit(number) {
-  const digit = document.createElement("div")
-  digit.classList.add("digit")
-
-  for (let i of Array(12).keys()) {
-    const segment = document.createElement("div")
-    segment.classList.add("segment")
-    segment.style.left = segmentsPositions[i].x
-    segment.style.top = segmentsPositions[i].y
-    segment.style.transform = `rotate(${segmentAngles[number][i]}deg)`
-
-    const dot = document.createElement("div")
-    dot.classList.add("dot")
-    segment.appendChild(dot)
-
-    digit.appendChild(segment)
-  }
-  addCoversToDigit(digit)
-
-  return digit
-}
-
-
-function addCoversToDigit(digit) {
+function addCoversToDigit(digit, segmentWidth) {
   const coverTop = document.createElement("div")
-  coverTop.classList.add("cover", "top")
+  coverTop.classList.add("cover")
+  coverTop.style.top = `-${segmentWidth * 0.7 + 1}px`
+  coverTop.style.left = `-${segmentWidth * 0.7}px`
+  coverTop.style.width  = `${segmentWidth * 2.7}px`
+  coverTop.style.height = `${segmentWidth * 0.7 + 1}px`
   digit.appendChild(coverTop)
 
   const coverBottom = document.createElement("div")
-  coverBottom.classList.add("cover", "bottom")
+  coverBottom.classList.add("cover")
+  coverBottom.style.top = `${segmentWidth * 2.3}px`
+  coverBottom.style.left = `-${segmentWidth * 0.7}px`
+  coverBottom.style.width  = `${segmentWidth*2.7}px`
+  coverBottom.style.height = `${segmentWidth * 0.7 + 1}px`
   digit.appendChild(coverBottom)
 
   const coverLeft = document.createElement("div")
-  coverLeft.classList.add("cover", "left")
+  coverLeft.classList.add("cover")
+  coverLeft.style.top = `-${segmentWidth * 0.7 }px`
+  coverLeft.style.left = `-${segmentWidth * 0.7 + 1}px`
+  coverLeft.style.width  = `${segmentWidth * 0.7 + 1}px`
+  coverLeft.style.height = `${segmentWidth * 3.7}px`
   digit.appendChild(coverLeft)
 
   const coverRight = document.createElement("div")
-  coverRight.classList.add("cover", "right")
+  coverRight.classList.add("cover")
+  coverRight.style.top = `-${segmentWidth}px`
+  coverRight.style.left = `${segmentWidth * 1.3}px`
+  coverRight.style.width  = `${segmentWidth * 0.7}px`
+  coverRight.style.height = `${segmentWidth * 3.7}px`
   digit.appendChild(coverRight)
 }
 
@@ -145,13 +173,13 @@ function setDigit(el, digit) {
 
 function findGetParameter(parameterName) {
   var result = null,
-      tmp = [];
+    tmp = []
   location.search
-      .substr(1)
-      .split("&")
-      .forEach(function (item) {
-        tmp = item.split("=");
-        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-      });
-  return result;
+    .substr(1)
+    .split("&")
+    .forEach(function(item) {
+      tmp = item.split("=")
+      if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1])
+    })
+  return result
 }
